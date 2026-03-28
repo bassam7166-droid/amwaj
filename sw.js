638cef1b5ef45232e74537c5690fba3f5f82a9d7
bassam7166-droid <shiama7166@gmail.com>
2026-03-28 11:28:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request));});
+const V='amwaj-v5';
+self.addEventListener('install',function(e){self.skipWaiting();});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(k){
+    return Promise.all(k.map(function(n){return caches.delete(n);}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(fetch(e.request).catch(function(){
+    return new Response('offline');
+  }));
+});
